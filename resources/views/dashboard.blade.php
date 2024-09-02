55f1e39d5d215d28cfa1c53bb0cb8d429fcde793
jamshidrajabov <jamshidamaliymatematika@gmail.com>
2024-09-02 14:42:45 +0500

    blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('teacher_app.blade.php')
+@section('title','ProEduCenter | Bosh sahifa')
+@section('breadcump')
+<div class="col-sm-6">
+    <h1 class="m-0">Bosh sahifa</h1>
+  </div><!-- /.col -->
+  <div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+      <li class="breadcrumb-item active"><a href="#">Bosh sahifa</a></li>
+    </ol>
+  </div><!-- /.col -->
+@endsection
+@section('content')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@endsection
